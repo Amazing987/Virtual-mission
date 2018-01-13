3412aa028bf1e810bd545d540798236d0941d92b
Amazing987 <35354448+Amazing987@users.noreply.github.com>
2018-01-13 23:10:21 +0530

    Add files via upload

---
+++ company.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class company
+{
+ public static void main(String args[])
+ {
+  class employee
+  {
+    int Eid,phno;
+    char em_name;
+    
+  
+  }
+
+  class department
+  {
+   char dp_name,designation;
+ 
+  }
+}
+}
+
+
+
+
+ 
